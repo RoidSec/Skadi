a710aa347cdc004fc099361ed9e2b8c2b5833edf
ChillinIX <colin.j.prater@outlook.com>
2023-02-09 15:16:34 +1000

    updates for wow classes

--- main.py
+++ main.py
@@ -1,38 +1,29 @@
-import discord
 import os
+import config
+import discord
 import asyncio
 from dotenv import load_dotenv
 from discord.ext import commands
+from discord import app_commands
 
 #intents
-client = commands.Bot(command_prefix="/", intents = discord.Intents.all())
 intents = discord.Intents.all()
 intents.message_content = True
-intents.members = True
+bot = commands.Bot(command_prefix=".", intents=intents)
 
-#login & sync commands
-@client.event
+@bot.event
 async def on_ready():
-    print(f"{client.user.name} is logged in.")
-    try:
-        synced = await client.tree.sync()
-        print(f"synced {len(synced)} command(s)")
-    except Exception as e:
-        print(e)
+    print('Online')
 
-#loading cogs
-@client.event
 async def load():
-  for file in os.listdir('cogs'):
-    if file.endswith('.py'):
-      await client.load_extension(f'cogs.{file[:-3]}')
+    for filename in os.listdir('./cogs'):
+        if filename.endswith('.py'):
+            await bot.load_extension(f'cogs.{filename[:-3]}')
 
-#first Command
-@client.tree.command(name="hello")
-async def hello(interaction:discord.Interaction):
-    await interaction.response.send_message(f"Hey, {interaction.user.mention}!")
+async def main():
+    await load()
+    load_dotenv()
+    token = os.getenv("discord_token")
+    await bot.start(token)
 
-#load token from secrets
-load_dotenv()
-token = os.getenv("discord_token")
-client.run(token)
+asyncio.run(main())
